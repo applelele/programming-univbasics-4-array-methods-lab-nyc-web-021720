b5d162d19b1d089744806699adde5f248f3db2ed
YUKIKO IKEDA <applelele@gmail.com>
2020-01-23 03:31:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,16 +11,20 @@ using_sort(["shirts", "pants", "Hat", "shoes"])
 def using_reverse(array)
   array.reverse
 end
-using_reverse()
+using_reverse(["Jpseph", "Mike", "Ryan", "Stephan"])
 
 def using_first(array)
-
+array.first
 end
+using_first(["dog", "cat", "bird", "rabbit"])
 
 def using_last(array)
-
+  
+array.last
 end
+using_last(["battery", "skrew", "soap", "bulb"])
 
 def using_size(array)
-
+array.size
 end
+using_size("Large", "Medium", "Small")
